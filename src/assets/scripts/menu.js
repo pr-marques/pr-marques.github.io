03a79d899d685f853ec2c221dd3a1b9f6ce286a3
pr-marques <pedronrm@gmail.com>
2025-06-01 12:19:22 +0100

    Update structure

--- src/assets/scripts/menu.js
+++ src/assets/scripts/menu.js
@@ -3,21 +3,3 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
-
-
-
-/* mobile menu */
-
-const hamburger = document.getElementById("hamburguer-container");
-const mobileMenu = document.getElementById("mobile-menu");
-const overlay = document.getElementById("overlay");
-
-function toggleMenu() {
-  const isOpen = mobileMenu.classList.contains("open");
-  mobileMenu.classList.toggle("open");
-  overlay.classList.toggle("active");
-  hamburger.classList.toggle("open");
-}
-
-hamburger.addEventListener("click", toggleMenu);
-overlay.addEventListener("click", toggleMenu);
