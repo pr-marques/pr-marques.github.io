552135ed4b8eb5dba5ab3a6f8bc2f7d33182d37a
pr-marques <pedronrm@gmail.com>
2025-06-06 14:41:50 +0100

    Adjusted About page responsiveness

--- src/assets/scripts/slideshow.js
+++ src/assets/scripts/slideshow.js
@@ -1,7 +1,8 @@
-let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
+let slideIndex = [1,1,1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3"]
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
