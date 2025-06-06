72a7148bda3472826242e2884bd20ec41d553d54
pr-marques <pedronrm@gmail.com>
2025-06-06 12:18:41 +0100

    Updated slideshows

--- src/assets/scripts/headerscroll.js
+++ src/assets/scripts/headerscroll.js
@@ -1,33 +1,3 @@
-    // header scroll tucks in
-
-    document.addEventListener("DOMContentLoaded", () => {
-        const topNav = document.querySelector("#main-header");
-        let lastScrollY = window.scrollY;
-        const threshold = 100;
-        let ticking = false;
-    
-        window.addEventListener("scroll", () => {
-            if (!ticking) {
-                window.requestAnimationFrame(() => {
-                    const currentScrollY = window.scrollY;
-    
-                    if (Math.abs(currentScrollY - lastScrollY) > threshold) {
-                        if (currentScrollY > lastScrollY) {
-                            topNav.classList.add("hidden");
-                        } else {
-                            topNav.classList.remove("hidden");
-                        }
-                        lastScrollY = currentScrollY;
-                    }
-                    ticking = false;
-                });
-                ticking = true;
-            }
-        });
-    });
-
-
-
     // header scroll background change
 
     window.addEventListener('scroll', () => {
